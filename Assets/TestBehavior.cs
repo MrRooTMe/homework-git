c27112c402ed17c067c5536bb305613d8e2046d4
avraham1233 <avraham1233@gmail.com>
2020-11-24 14:22:41 +0200

    first one

---
+++ Assets/TestBehavior.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject myGameObject = new GameObject("Test Object"); // Make a new GO.
+        Rigidbody gameObjectsRigidBody = myGameObject.AddComponent<Rigidbody>(); // Add the rigidbody.
+        gameObjectsRigidBody.mass = 5; // Set the GO's mass to 5 via the Rigidbody.  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
